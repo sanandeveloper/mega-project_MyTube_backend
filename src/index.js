1033607990581f0e81541b850d61591d9471e1bf
sanandeveloper <sanankhan201516@gmail.com>
2025-11-05 18:07:18 +0500

    updated code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ dotenv.config({
   path: "./.env",
 });
 
-
+import app from "./app.js";
 
 import connectDB from "./db/db.js";
-
+const PORT = process.env.PORT || 7000;
 
 connectDB()
   .then(() => {
-    console.log("database connected");
+    // const app = express();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+    console.log("first");
   })
   .catch((err) => {
     console.log("error account while database connect to express", err);
