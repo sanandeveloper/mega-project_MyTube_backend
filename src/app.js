7e0ee760ce974a0dbd628a6ecc5591a015f58dad
sanandeveloper <sanankhan201516@gmail.com>
2025-11-05 17:32:40 +0500

    updated code

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "./routes/user.routes.js";
-import dotenv from "dotenv"
-dotenv.config()
-// const PORT = process.env.PORT || 7000;
-
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 7000;
 
 const app = express();
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 7000 ,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || 7000,
+  }),
+);
 app.use(
   express.json({
     limit: "16kb",
@@ -20,18 +21,16 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
-console.log("hello world")
+console.log("hello world");
 
-
-app.use("/api/v1/users",router);
+app.use("/api/v1/users", router);
 
 import videoRouter from "./routes/video.routes.js";
-app.use("/api/v1/video",videoRouter)
+app.use("/api/v1/video", videoRouter);
 import { subsRoute } from "./routes/subscriber.routes.js";
-app.use("/api/v1/subscriber",subsRoute)
-// app.listen(PORT,()=>{
+app.use("/api/v1/subscriber", subsRoute);
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
 
-//   console.log("APP IS RUNNING ON THIS PORT")
-// })
-
-export default app
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config({
-  path:'./.env'
-})
-
-
- 
-import app from "./app.js"
-
-import connectDB from "./db/db.js";
-  const PORT = process.env.PORT || 7000;
-
-
-connectDB().then(() => {
-
-  // const app = express();
-    app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
-  });
-  console.log('first')
-
-}).catch((err)=>{
-   console.log('error account while database connect to express',err)
+  path: "./.env",
 });
 
+import app from "./app.js";
 
+import connectDB from "./db/db.js";
+const PORT = process.env.PORT || 7000;
+
+connectDB()
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log("error account while database connect to express", err);
+  });
