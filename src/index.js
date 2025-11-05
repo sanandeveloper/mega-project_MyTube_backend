1edfe964bd9ec095982bbd71f6f0b40a7aa70587
sanandeveloper <sanankhan201516@gmail.com>
2025-11-05 17:47:56 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ dotenv.config({
   path: "./.env",
 });
 
-import app from "./app.js";
+
 
 import connectDB from "./db/db.js";
-const PORT = process.env.PORT || 7000;
+
 
 connectDB()
   .then(() => {
